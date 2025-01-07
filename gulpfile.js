07c96aa4d1566b7b103f9616c2c0eb5836c0452d
Luiz Conde <luizconde.dev@outlook.com>
2025-01-07 16:49:04 -0300

    Add Image and Minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 function styles(){
-    return gulp.src()
+    return gulp.src('./src/styles/*.scss')
     .pipe(sass())
     .pipe(cleanCSS())
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images(){
+    return gulp.src('./src/imgs/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/imgs'));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
