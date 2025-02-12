67f9598fd07441aff145fc2f1773e23459d57052
Luiz Conde <luizconde.dev@outlook.com>
2025-02-12 02:23:51 -0300

    Add FAQ section with interactive questions and answers

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
 
     for (let i  = 0; i < buttons.length; i++){
@@ -10,11 +11,19 @@ document.addEventListener('DOMContentLoaded', function(){
             aba.classList.add('shows__list--is-active');
             removeBotaoAtivo();
             button.target.classList.add('shows__tabs__button--is-active');
-            
-
         })
     }
-})
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+}})
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function escondeTodasAbas(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
