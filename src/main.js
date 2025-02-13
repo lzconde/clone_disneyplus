3532a6ea66a883a9658331c18565707f710afc41
Luiz Conde <luizconde.dev@outlook.com>
2025-02-13 03:15:14 -0300

    Add header with visibility toggle on scroll

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    
 
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight; 
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual >= heroHeight){
+            exibeElementosDoHeader();
+        }else{
+            ocultaElementosDoHeader();
+        }
+    });
+    
+    //Seção de Atrações, Programação das Abas
     for (let i  = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(button){
             const abaAlvo = button.target.dataset.tabButton;
@@ -13,10 +26,23 @@ document.addEventListener('DOMContentLoaded', function(){
             button.target.classList.add('shows__tabs__button--is-active');
         })
     }
-
+    //Seção FAQ, accordions
     for(let i = 0; i < questions.length; i++){
         questions[i].addEventListener('click', abreOuFechaResposta);
 }})
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+
+    header.classList.remove('header--is-hidden');
+}
+
 
 function abreOuFechaResposta(elemento){
     const classe = 'faq__questions__item--is-open';
