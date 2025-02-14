e5af6ac6fd6d0f803326100320776dc18a94f4fd
Luiz Conde <luizconde.dev@outlook.com>
2025-02-13 21:41:26 -0300

    Add Gulp task for JavaScript minification and restructure scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -16,7 +23,8 @@ function images(){
     .pipe(gulp.dest('./dist/imgs'));
 }
 
-exports.default = gulp.parallel(images, styles);
+exports.default = gulp.parallel(images, styles, scripts);
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
